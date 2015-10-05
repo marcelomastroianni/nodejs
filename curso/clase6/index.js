66fe7f981056a73cd9cde49491852ff9f5afbb59
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2015-10-05 19:53:55 -0300

    Update index.js

--- curso/clase6/index.js
+++ curso/clase6/index.js
@@ -17,11 +17,47 @@ var app = express();
 
 
 app.get('/', function(req,res){
-
-        res.send("Hello Express!!");
-    
-    
+    res.send("Hello Express!!");    
 });
+
+
+
+//Este listener es mucho mas especifico que '/hello/:name' porque machea con muchos mas casos, entonces si queremos que se ejecute
+//debemos ponerlo antes que '/hello/:name',
+//En express el orden de las rutas importa
+
+app.get('/hello/*',function(req,res,next){
+   console.log("REQUEST EN /HELLO/");
+    console.log();
+});
+
+
+
+
+//El parametro : es el nombre de una variable.
+//Tiene un motor que utiliza expressiones regulares.
+//Se parsea la ruta
+//Podria tener una url que tenga el id del recurso que quiero obtener
+
+
+/*
+Cuando queremos leer la siguiente pagina el nevagador web:
+
+http://localhost:3000/hello/alguien
+
+Otenemos
+
+Hello, alguien
+*/
+
+
+app.get('/hello/:name', function(req,res){
+    res.send("Hello, " + req.params.name);    
+});
+
+
+
+
 
 
 var server = app.listen(3000, function(){
