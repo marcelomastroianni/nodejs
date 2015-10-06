df0ad9bb626dc3d5f266015e826ac77657cdcfe0
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2015-10-05 21:45:52 -0300

    Update html.js

--- curso/clase6/html-template/html.js
+++ curso/clase6/html-template/html.js
@@ -2,6 +2,9 @@ var express = require('express');
 var fs = require("fs");
 var app = express();
 
+app.engine('html',require('ejs').renderFile);
+app.set('view engine', 'html');
+
 //Implementamos un cache para el archivo json
 var users = null;
 var getUsers = function(callback){
@@ -18,22 +21,21 @@ var getUsers = function(callback){
   }   
 };
 
-app.get('/users/',function(req,res,next){
+
+app.get("/",function(req,res){
+     res.render('index',{tittle: "Intro Express"});
+});
+
+
+
+app.get('/users/',function(req,res){
+                
     getUsers(function(err,data){        
         
-        //El array va a tener la misma cantidad de elemenetos que el array original,
-        //pero en cada item voy a tener lo que devuelva la funcion que recibe map
-        
-        var list = data.map(function(user){
-            return "<li><a href='/user/" + user._id + "' >" + user.name + "</a></li>"
-        });
-        
-        //por defecto devuelve html
-        res.send("<ul>" + list.join("") + "</ul>" );        
-        
-        
+        res.render('users',{users:data });
         
     });
+    
 });
 
 app.get('/user/:id', function(req,res){
@@ -42,18 +44,22 @@ app.get('/user/:id', function(req,res){
          
          //La funcion callback de reduce se ejecuta por cada uno de los items del array
          //El parametro que 
-        var response = data.reduce(function(defaultRes,user){
+        var user = data.reduce(function(defaultRes,user){
             
             //Utilizamos el operador ternario
             //Si se cumple la condicion devuelvo el usuario
             //Si no se cumple la condicion devuelvo defaultRes, que en este caso es el valro con el que
             //inicializo reduce
-            return user._id == req.params.id ? "<h1> "+ user.name  +  " </h1>" :  defaultRes;
-        }, "<h1>Usuario inexistente</h1>");
+            return user._id == req.params.id ? user :  defaultRes;
+        },null);
+         
+         if(!user)
+            res.render('error');
+         
+         res.render('user',{user:user});
+         
          
         
-         //por defecto devuelve html
-        res.send(response);
          
     });
     
