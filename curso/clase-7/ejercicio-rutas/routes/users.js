90faa2224cbb9bfa2a497d076ec04ec4da096c16
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2015-10-19 21:59:25 -0300

    Create users.js

---
+++ curso/clase-7/ejercicio-rutas/routes/users.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var userModel = require("../models/user");
+
+
+//Los params de la aplicacion principal no llegan aca directamente
+//a menos que mergeemos los parametros.
+//Ya que es una aplicacion aparte del router que esta en app.
+
+
+router.post('/', function(req, res){    
+    userModel.create(req.body,function(err,data){
+       res.json(data); 
+    });    
+});
+
+
+router.get('/', function(req, res){
+   userModel.list(function(err,data){
+       res.json(data); 
+    });    
+});
+
+router.get('/:id', function(req, res){
+  userModel.get(req.params.id,function(err,data){
+       res.json(data); 
+    });    
+});
+
+
+router.delete('/:id', function(req, res){
+  userModel.delete(req.params.id,function(err,data){
+       res.json(data); 
+    });    
+});
+
+
+router.put('/', function(req, res){
+  userModel.update(req.body,function(err,data){
+       res.json(data); 
+    });    
+});
+
+
+//Tengo que pisar exports
+module.exports = router;
