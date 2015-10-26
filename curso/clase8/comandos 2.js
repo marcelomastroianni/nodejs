2a11584370fc600abc1c208de58f63a27b258d2b
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2015-10-26 20:01:50 -0300

    Update comandos 2.js

--- curso/clase8/comandos 2.js
+++ curso/clase8/comandos 2.js
@@ -1,3 +1,12 @@
 
 
-mongoimport --db test --colection products --file products.json
+mongoimport --db test --collection products --file products.json
+
+
+
+db.products.find({});
+
+
+trae todos los productos con el campo desc numerico superior a 10,
+trae solo el campo name de cada documento.
+db.products.find({desc: {$gte : 10}},{name: 1});
