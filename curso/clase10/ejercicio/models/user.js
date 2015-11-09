5cd324ecae4adeb62e4c488480bb6cba40b5fb0f
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2015-11-09 20:00:57 -0300

    Update user.js

--- curso/clase10/ejercicio/models/user.js
+++ curso/clase10/ejercicio/models/user.js
@@ -1,55 +1,78 @@
-var con = require('./connection');
-var DB_COLLECTION = 'users';
+var mongoose = require('mongoose');
 
-exports.create = function(data, callback){
-  con.connect(function(err, db){
-    var collection = db.collection(DB_COLLECTION);
-    collection.insertOne(data, function(err, result){
-      console.log(result);
-      callback(null, result);
-    });
-  });
-};
 
-exports.update = function(data, callback){
-  data._id = con.ObjectID(data._id);
-  con.connect(function(err, db){
-    var collection = db.collection(DB_COLLECTION);
-    collection.updateOne({ _id : data._id }, data, function(err, result){
-      console.log(result);
-      callback(null, result);
-    });
-  });
-};
+//No es necesario definir el nombre de la conexion
+//Cuando creo el Schema se va a crear la collecion con el nombre del Schema
+//El modelo se basa en el Schema
+//El modelo es un contructor de un objeto de un tipo de un Schema particular
+//Un objeto es una instancia del modelo, que tambien tiene metodos CRUD del objeto, y no de todo el modelo.
 
-exports.delete = function(id, callback){
-  var oid = con.ObjectID(id);
-  con.connect(function(err, db){
-    db.collection(DB_COLLECTION)
-    .deleteOne({ _id : oid }, function(err, result){
-      console.log(result);
-      callback(null, result);
-    });
-  });
-};
+//La parte de la conexion se maneja en segundo plano, yo defino el schema y mongoose se va a encargar de la conexion y ejecutar los 
+//comandos correspondientes.
 
-exports.list = function(callback){
-  con.connect(function(err, db){
-    db.collection(DB_COLLECTION)
-    .find({}).toArray(function(err, result){
-      console.log(result);
-      callback(null, result);
-    });
-  });
-};
+//va a necesitar una definicion de las properties que va a tener nuestra colleccion.
+var userSchema = new mongoose.Schema({
+   
+    /*
+    lo que le estoy pasando es el constructor nativo de Javascript
+    firstName: String,
+    */
+    firstName : {
+       type: String,
+       require: true
+    },
+    lastName : {
+       type: String,
+       require: true
+    },
+    userName : {
+       type: String,
+       require: true
+    },
+    /*
+    Se comporta como un id, si inserto un email que ya fue ingresado va a explotar.
+    Y tambien crea un indice para el campo email.
+    Esto por tener la propiedad unique en true.
+    */
+    email : {
+       type: String,
+       unique: true,
+       require: true
+    },
+    createdOn : {
+       type: Date,
+       default: Date.now
+    }
+    
+    
+});
 
-exports.get = function(id, callback){
-  var oid = con.ObjectID(id);
-  con.connect(function(err, db){
-    db.collection(DB_COLLECTION)
-    .findOne({ _id : oid }, function(err, result){
-      console.log(result);
-      callback(null, result);
-    });
-  });
-};
+//Ahora que tengo el Schema puedo crear el modelo.
+
+//Esto lo que me devuelve es un constructor de User
+//Tiene metodos estaticos.
+//El schema no lo uso mas en ningun lado
+
+//exports = mongoose.model('User',userSchema);
+
+
+/*
+exports apuntaba a:
+
+module = {
+    exports: ,
+    otras variables : 
+}
+
+exports = module.exports
+
+Si yo lo que quiero es exportar un objeto que tenga muchos metodos lo que hago es,
+
+exports.User = mongoose.model('User',userSchema);
+
+pero en este caso agregamos un nivel mas que no es necesario.
+
+module.exports
+*/
+
+module.exports = mongoose.model('User',userSchema);
