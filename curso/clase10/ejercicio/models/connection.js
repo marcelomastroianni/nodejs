52367b976afdc7e87c850b828d14d696feb9ead4
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2015-11-09 19:13:37 -0300

    Create connection.js

---
+++ curso/clase10/ejercicio/models/connection.js
@@ -0,0 +1,26 @@
+var mongo = require('mongodb');
+var client = mongo.MongoClient;
+var dbURL = 'mongodb://localhost:27017/userapi';
+var db = null;
+
+exports.connect = function(callback){
+  
+  if(db){
+    return process.nextTick(function(){
+      callback(null, db);
+    });
+  }
+  
+  client.connect(dbURL, function(err, database){
+    if(err){
+      return callback(err);
+    }
+    db = database;
+    callback(null, db);
+  });
+  
+};
+
+exports.ObjectID = function(hash){
+  return new mongo.ObjectID(hash);
+};
